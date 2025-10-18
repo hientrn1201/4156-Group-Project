c44b3869d061a8768ad8d7e562735acc727eaae2
Christopher Ng <chrisng@christophersmbp.myfiosgateway.com>
2025-10-18 16:28:05 -0400

    small changes:
    - groupid/artifactid/name changed for project as was using individualproject template

--- src/main/java/com/team/knowledge/controller/Controller.java
+++ src/main/java/com/team/knowledge/controller/Controller.java
@@ -2,12 +2,14 @@ package com.team.knowledge.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping("/api")
 public class Controller {
-    @GetMapping({"/","/index"})
+    @GetMapping({"","/"})
     public ResponseEntity<String> index() {
         return ResponseEntity.ok("Welcome to Knowledge Management Service Powered by AI!");
     }
